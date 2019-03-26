818e567d660d8bfe97b18ba15ab5f4ad46bd7ad7
korizza <deykunov@live.com>
2019-03-27 01:32:39 +0300

    Removed unnecessary hook

--- src/main/java/org/korizza/colorizer/demo/App.java
+++ src/main/java/org/korizza/colorizer/demo/App.java
@@ -10,14 +10,7 @@ public class App {
         ui.setVisible(true);
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        final long start = System.nanoTime();
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            public void run() {
-                System.out.format("HOOKED");
-            }
-        }));
-
+    public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             App app = new App();
         });
